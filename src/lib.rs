4421302d95f890f7228178ec47304e62597251ff
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-09-07 01:00:47 +0300

    Export RocksDB Raft persistence

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub mod mvcc;
 pub mod optimizer;
 pub mod protocol;
 pub mod query_executor;
+pub mod raft_persistence;
 pub mod replicated_sql;
 pub mod replicated_state_machine;
 pub mod rocksdb_catalog;
