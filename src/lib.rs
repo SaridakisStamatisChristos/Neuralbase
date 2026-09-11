687bc7fe163370140e04754ccc95e7829b71f70c
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-09-11 20:12:57 +0300

    phase6: export read consistency contract

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ pub mod optimizer;
 pub mod protocol;
 pub mod query_executor;
 pub mod raft_persistence;
+pub mod read_consistency;
 pub mod replicated_gateway;
 pub mod replicated_identity;
 pub mod replicated_identity_migration;
